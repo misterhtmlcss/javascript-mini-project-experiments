69453dc34629a63604efc9ea5c5dd7b9d4bacf24
Roger K <pythonlabsstudent@users.noreply.github.com>
2015-11-05 00:22:01 -0700

    Revision based on first round of feedback with Anthony
    Revised to include a 'return' and compressed my calls and methods below.

--- dividend-ROI.js
+++ dividend-ROI.js
@@ -1,20 +1,25 @@
-
+// My favourite version:
 var myLIQ = function(stockPrice, divYield, keepTime) {
     this.stockPrice = stockPrice;
     this.divYield = divYield;
     this.keepTime = keepTime;
+	return stockPrice * divYield * keepTime;
 }
-stockPrice = prompt("What is the stock price? " + "");
-divYield = prompt("How many shares do you get granted? " + "");
-keepTime = prompt("What time period will you hold these shares? " + "");
-var totalReturn = myLIQ((stockPrice * divYield) * keepTime); 
-document.write(totalReturn);
-Basically I had written it where it was like this too:
-var myLIQ = function(stockPrice, divYield, keepTime) {
-    this.stockPrice =  prompt("What is the stock price? " + "");
-    this.divYield = prompt("How many shares do you get granted? " + "");
-    this.keepTime = prompt("What time period will you hold these shares? " + "");
-}
-var totalReturn = myLIQ((stockPrice * divYield) * keepTime); 
+var stockPrice = parseInt(prompt("What is the stock price? " + ""));
+var divYield = parseInt(prompt("How many shares do you get granted? " + ""));
+var keepTime = parseInt(prompt("What time period will you hold these shares? " + ""));
+
+var totalReturn = myLIQ(stockPrice, divYield, keepTime); 
 document.write(totalReturn);
 
+/* --------------------- variation line ---------------------- */
+
+// Here is variation b (I just tried it and it works...Not sure it's the best way though):
+var myLIQ = function(stockPrice, divYield, keepTime) {
+    this.stockPrice = stockPrice;
+    this.divYield = divYield;
+    this.keepTime = keepTime;
+	return stockPrice * divYield * keepTime;
+}
+var totalReturn = myLIQ(parseInt(prompt("What is the stock price? " + "")), parseInt(prompt("How many shares do you get granted? " + "")), parseInt(prompt("What time period will you hold these shares? " + "")));
+document.write(totalReturn);
